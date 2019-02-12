6339d55790cea51e118c2f4c9932ceddd3338c18
Roshini Jaiswal <roshini.s.jaiswal@gmail.com>
2019-02-12 23:39:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "f526456285e9427ac1c47747b532b501"
+
+//1. We need an event listener for the input box('submit')
+//2. Get what they types in, and then Fetch data from the weather API for that city
+//3. Fill out the forecast/graph
 
 function handleFormSubmit(event) {
   //handle submit event
+  event.preventDefault()
+  //Get the input that they typed in
+  const input = document.querySelector('#city')
+  // use the .value to get the text
+  const whatTheyTyped = input.value
+  console.log(whatTheyTyped)
 }
 
 function fetchCurrentWeather(city) {
@@ -15,6 +25,7 @@ function displayCurrentWeather(json) {
 
 function fetchFiveDayForecast(city) {
   //fetch five day forecast data based on city
+  
 }
 
 function displayFiveDayForecast(json) {
@@ -23,8 +34,37 @@ function displayFiveDayForecast(json) {
 
 function createChart(json) {
   //Bonus: render temperature chart using five day forecast data and ChartJS
+  const ctx = document.getElementById('WeatherChart').getContext('2d')
+  var my chart= new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   //add event listener here for form submission
+  document.addEventListener('submit',handleFormSubmit)
 })
